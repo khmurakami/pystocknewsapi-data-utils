291026709f260845222d782b76ec2c5fa64e096a
unknown <kalanimurakami1218@gmail.com>
2019-06-15 18:14:38 -0700

    Added Template code, with gcp-client repo

--- pystocknewsapi_data_utils/pystocknewsapi_data_utils.py
+++ pystocknewsapi_data_utils/pystocknewsapi_data_utils.py
@@ -1,107 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests
-import os
+from __future__ import unicode_literals
+from pystocknewsapi import NewsStreamer
 
 # Insert your own key as a string here
 API_KEY = os.environ['STOCK_NEWS_API_KEY']
 
-class NewsStreamer():
+class PyStockNewsAPIData():
 
     def __init__(self, API_KEY):
 
-        if API_KEY is None:
-            raise Exception("No API KEY was passed in")
+        self.streamer = NewsStreamer(API_KEY)
 
-        self.root_url = "https://stocknewsapi.com/api/v1"
-        self.client_id = API_KEY
 
     # https://www.mapillary.com/developer/api-documentation/#pagination
-    def get_ticker_news(self, tickers="", items=20, data="", type="article",
-                        source="CNBC", sentiment="neutral", tag="CEO",
-                        tagexclude="CEO", fallback="true", page=1,
-                        sortby="trending"):
+    def get_recent_general_news_article(self, source):
 
-        """Get news based on the specified company('s')
+        """Get the most recent general news article based on source
 
         Args:
-            tickers (string): Name of the specified company
-            items (int): Number of news items returned 1-50
-            date (string): Format, DDMMYYYY - DDMMYYYY
-            type (string): Video or Article
-            source (string): Name of news source
-            sentiment (string): positive, negative or neutral sentiment
-            tag (string): Name of the tag you want to include
-            tagexclude (string): Name of the you want to exclude
-            page (int): Amount of pages you want
-            sortby (string): Sort by this parameter
+            source
 
         Return:
-            raw_json (dict): Dictionary of the result json requested
-
-        Raises:
-            Exception error: Uses HTTP error handler to check status code
+            recent_news (json dict): The most recent news as a dict
 
         """
 
-        url = self.root_url
+        raw_json = self.streamer.get_general_market_news()
+        recent_news = raw_json["data"][0]
 
-        data = {
-                 'tickers' : '{}'.format(tickers),
-                 'items' : '{}'.format(items),
-                 'date': '{}'.format(date),
-                 'type': '{}'.format(type),
-                 'source': '{}'.format(source),
-                 'sentiment': '{}'.format(sentiment),
-                 'tag': '{}'.format(tag),
-                 'tagexclude': '{}'.format(tagexclude),
-                 'page': '{}'.format(page),
-                 'sortby': '{}'.format(sortby),
-                 'token': '{}'.format(self.client_id)
-                }
+        return recent_news
 
-        r = requests.get(url, params=data)
-        http_error_handler(r.status_code)
-        raw_json = r.json()
-        return raw_json
+    def stocknewsapi_csv_list_create(self, csv_name, ):
 
-    def get_general_market_news(self, section="general", items=20,
-                                type="article", source="CNBC",
-                                sourceexclude="benzinga", sentiment="neutral"):
-
-        """Get general market news
+        """Create a CSV dataset based on the source
 
         Args:
-            section (string): Section of the market news you want
-            items (int): Number of news items returned 1-50
-            type (string): Video or Article
-            source (string): Name of news source
-            sourceexclude (string): Name of the source you want to exclude
-            sentiment (string): positive, negative or neutral sentiment
+            csv_name (string): Name of the csv you want to create
+
 
         Return:
-            raw_json (dict): Dictionary of the result json requested
-
-        Raises:
-            Exception error: Uses HTTP error handler to check status code
+            None
 
         """
 
-        url = self.root_url
-
-        data = {
-                 'section' : '{}'.format(section),
-                 'items' : '{}'.format(items),
-                 'date': '{}'.format(date),
-                 'type': '{}'.format(type),
-                 'source': '{}'.format(source),
-                 'sourceexclude': '{}'.format(sourceexclude),
-                 'sentiment': '{}'.format(sentiment),
-                 'token': '{}'.format(self.client_id)
-                }
-
-        r = requests.get(url, params=data)
-        http_error_handler(r.status_code)
-        raw_json = r.json()
-        return raw_json
+        pass
